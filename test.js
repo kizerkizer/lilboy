21d277097f4fd2de2342758f9931fdf11017c95a
Alex Kizer <apkizer2@illinois.edu>
2017-09-23 20:48:40 +0000

    add esprima; other changes

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const acorn = require('acorn'),
+      esprima = require('esprima'),
       sbs = require('sidebyside');
 
 const tokenizer = require('./dist/out.js');
@@ -25,28 +26,42 @@ function (foo, bar) {
 }
 `;
 
-const tabs = fs.readFileSync('./samples/across-tabs.js').toString();
-const somejqueryfile = fs.readFileSync('./samples/somejqueryfile.js').toString();
-const anotherjqueryfile = fs.readFileSync('./samples/anotherjqueryfile.js').toString();
-const jquery = fs.readFileSync('./samples/jquery.min.js').toString();
+//const tabs = fs.readFileSync('./samples/across-tabs.js').toString();
+//const somejqueryfile = fs.readFileSync('./samples/somejqueryfile.js').toString();
+//const anotherjqueryfile = fs.readFileSync('./samples/anotherjqueryfile.js').toString();
+//const jquery = fs.readFileSync('./samples/jquery.js').toString();
+const react = fs.readFileSync('./samples/react.js').toString();
 
-const test = anotherjqueryfile;
+const test = react;
 
 
 let a = [],
-    b = [];
+    b = [],
+    c = [];
 
+console.log('lilboy');
 let s = Date.now();
 let n = 0;
 let tokens;
 tokens = tokenizer(test);
-  for (token of tokens) { a.push(token.value); n++; }
+  for (token of tokens) { a.push(token); n++; }
 console.log(`${Date.now() - s}ms; ${n} tokens`);
+console.log('');
 
-s = Date.now();
+console.log('acorn');
 n = 0;
+s = Date.now();
 tokens = acorn.tokenizer(test);
-  for (token of tokens) { b.push(token.type.label); n++; }
+  for (token of tokens) { b.push(token); n++; }
 console.log(`${Date.now() - s}ms; ${n} tokens`);
+console.log('');
+
+console.log('esprima');
+n = 0;
+s = Date.now();
+tokens = esprima.tokenize(test);
+  for (token of tokens) { c.push(token); n++; }
+console.log(`${Date.now() - s}ms; ${n} tokens`);
+console.log('');
 
 //sbs(a, b);
