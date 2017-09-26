bee061559618aca6b45b5737ec4c347baf0aaa55
kizerkizer <alex@kizer.com>
2017-09-26 22:47:01 +0000

    remove src

--- src/readComment.js
+++
@@ -1,5 +0,0 @@
-if (this.input.charCodeAt(this.currentIndex) ===  47 && this.input.charCodeAt(this.currentIndex + 1) === 47) {
-  comment = this.input.slice(this.currentIndex, this.input.indexOf('\n', this.currentIndex)); // TODO
-} else {
-  comment = null;
-}
