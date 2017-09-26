1cf598ea1017b2d1aabdae95915dd6e25f4d9838
kizerkizer <alex@kizer.com>
2017-09-26 22:58:06 +0000

    update parser

--- parser.js
+++ parser.js
@@ -1,15 +1,1 @@
-const fs = require('fs');
-
-const tokenizer = require('./tokenizer');
-
-const code = `
-function (foo) {
-  return foo + 5;
-}
-`;
-
-let tokens = tokenizer(code);
-
-function parseExpression () {
-  
-}
+// TODO
